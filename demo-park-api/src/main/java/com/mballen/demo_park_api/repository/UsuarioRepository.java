7c86c9c1a542e5bf6d45b876cc76b231c00b6561
Guilhermeazevedo1 <guilhermeazevedo_2603@hotmail.com>
2025-08-06 08:39:48 -0300

    atualização do código

---
+++ demo-park-api/src/main/java/com/mballen/demo_park_api/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.mballen.demo_park_api.repository;
+
+import com.mballen.demo_park_api.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
